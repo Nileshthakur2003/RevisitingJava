9a5123980ffa953b14d2781bb88a02c2511cb1fd
Nilesh1927 <nileshthakurbgp@gmail.com>
2024-04-17 14:10:04 +0530

    Create java_sum.java

---
+++ java_sum.java
@@ -0,0 +1,9 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Integer a = 61;
+        Integer b = 72;
+        Integer c = a + b;
+        System.out.println("Sum is "+c);
+    }
+}
